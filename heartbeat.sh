aef7ea1e3522fbef19577de3c71142a13c08330f
pi <naomi@aktivix.org>
2016-10-25 13:28:05 +0000

    add interface sample, updated heartbeat to test interface

--- heartbeat.sh
+++ heartbeat.sh
@@ -2,10 +2,23 @@
 
 identity=`grep -o '^\S*' /home/pi/zabbix/identity.txt` # don't allow spaces
 server=`grep -o '^\S*' /home/pi/zabbix/server.txt` # don't allow spaces
+interface=`grep -o '^\S*' /home/pi/zabbix/interface.txt`
+
+# a host on the internet that should always respond to ping
+host=8.8.8.8
 
 echo -e "\n\n"
-date
-echo "Heartbeat"
+
 send_cmd="zabbix_sender -z $server -s $identity --tls-connect psk --tls-psk-identity $identity --tls-psk-file /home/pi/zabbix/psk.txt"
 
-$send_cmd -k heartbeat.up -o "`date`"
+aps_down=`fping -I $interface -uq -t500 $host`
+
+if [ ${#aps_down} -ne 0 ]; then
+    output="DOWN $interface `date`"
+else
+    output="OK $interface `date`"
+fi
+
+echo "Heartbeat: $output"
+
+$send_cmd -k heartbeat.up -o "$output"
